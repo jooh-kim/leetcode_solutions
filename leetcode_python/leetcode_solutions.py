a864c097cab4bda3b117815446d1244165dc047b
jkim1 <Joohyung.Kim@amd.com>
2019-10-29 11:33:23 -0400

    add 1 more

--- leetcode_python/leetcode_solutions.py
+++ leetcode_python/leetcode_solutions.py
@@ -1,4 +1,5 @@
 import math
+import sys
 """ 
 Problem: Movies On Flight 
 
@@ -491,6 +492,36 @@ def canJumpHelper(nums, index, steps):
             index += steps
             return canJumpHelper(nums, index,  steps)
 
+""" 
+Say you have an array for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+"""
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    if len(prices) < 2:
+        return 0
+    max_profit = 0
+    lowest_price = sys.maxsize
+    for p in prices:
+        if p < lowest_price:
+            lowest_price = p
+        else:
+            # current price is higher than the lowest_price
+            if p - lowest_price > max_profit:
+                max_profit = p - lowest_price
+    return max_profit
+
 
 def main():
     # result = []
@@ -501,7 +532,7 @@ def main():
     # s = "babaddtattarrattatddetartrateedredividerb"
     # s = "bb"
     # print(longestPalindrome(s))
-    print(canJump([2,0,0]))
+    print(maxProfit([1,2,3,1,1,1,1,3,2,1]))
 
 if __name__== "__main__":
     main()
