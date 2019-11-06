0678abf3beaa606aae18253b595c9d0e33453b18
jkim1 <Joohyung.Kim@amd.com>
2019-11-06 16:41:44 -0500

    1 more solution

--- leetcode_python/leetcode_solutions.py
+++ leetcode_python/leetcode_solutions.py
@@ -592,6 +592,42 @@ def rotateRight(head, k):
     temp.next = None
     return head.next
 
+def romanToInt(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    if s == "":
+        return 0
+    i = 0
+    result = 0
+    while i < len(s):
+        if i + 1 < len(s):
+            if s[i] == "I" and (s[i+1] == "V" or s[i+1] == "V"):
+                result = result + roman[s[i+1]] - roman[s[i]]
+                i += 2
+            elif s[i] == "X" and (s[i+1] == "L" or s[i+1] == "C"):
+                result = result + roman[s[i+1]] - roman[s[i]]
+                i += 2
+            elif s[i] == "C" and (s[i+1] == "D" or s[i+1] == "M"):
+                result = result + roman[s[i+1]] - roman[s[i]]
+                i += 2
+            else:
+                result = result + roman[s[i]]
+                i += 1
+        else:
+            result = result + roman[s[i]]
+            i += 1
+    return result
 
 def main():
     # result = []
@@ -617,6 +653,8 @@ def main():
     while answer != None:
         print(answer.val)
         answer = answer.next
+
+    print("answer: ", romanToInt("XXXLIV"))
 
 if __name__== "__main__":
     main()
